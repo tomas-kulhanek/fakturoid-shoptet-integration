156c6f71407d254b3ffc29e690d01306e1bf9443
Tomáš Kulhánek <jsem@tomaskulhanek.cz>
2021-10-04 09:21:01 +0200

    Persist projektu

--- src/Manager/ProjectManager.php
+++ src/Manager/ProjectManager.php
@@ -7,6 +7,7 @@ namespace App\Manager;
 
 use App\Api\ClientInterface;
 use App\Database\Entity\Shoptet\Project;
+use App\Database\Entity\User;
 use App\Exception\Logic\NotFoundException;
 use App\Facade\UserRegistrationFacade;
 use Doctrine\ORM\EntityManagerInterface;
@@ -17,7 +18,8 @@ class ProjectManager
 		private ClientInterface $apiDispatcher,
 		private EntityManagerInterface $entityManager,
 		private UserRegistrationFacade $userManager
-	) {
+	)
+	{
 	}
 
 	public function getByEshopId(int $eshopId): Project
@@ -39,25 +41,31 @@ class ProjectManager
 			$project = new Project();
 			$this->entityManager->persist($project);
 		}
-		try {
-			$userEntity = $this->userManager->findOneByEmail($installationData->contactEmail);
-			$userEntity->addProject($project);
-		} catch (NotFoundException) {
-			$userEntity = $this->userManager->createUser(
-				$installationData->contactEmail,
-				$project
-			);
-			$project->setOwner($userEntity);
-		}
-
-
 		$project->setAccessToken($installationData->access_token);
 		$project->setContactEmail($installationData->contactEmail);
 		$project->setEshopId($installationData->eshopId);
 		$project->setEshopUrl($installationData->eshopUrl);
 		$project->setScope($installationData->scope);
 		$project->setTokenType($installationData->token_type);
-		$this->entityManager->persist($project);
+
+		try {
+			$userEntity = $this->userManager->findOneByEmail($installationData->contactEmail);
+			$userEntity->addProject($project);
+		} catch (NotFoundException) {
+
+			try {
+				$this->userManager->findOneByEmail($installationData->contactEmail);
+				//todo co ted?
+			} catch (NotFoundException) {
+				$userEntity = new User(
+					email: $installationData->contactEmail,
+					project: $project
+				);
+				$project->setOwner($userEntity);
+				$this->entityManager->persist($userEntity);
+			}
+		}
+
 		$this->entityManager->flush();
 		return $project;
 		//odpalit do rabbita!
