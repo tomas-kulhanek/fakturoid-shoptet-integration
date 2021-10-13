9966d1e2ffdd9f6569a434fa1609ae6408164bc8
Tomáš Kulhánek <jsem@tomaskulhanek.cz>
2021-10-13 21:46:25 +0200

    Oprava nette syntax

--- src/MessageBus/EshopDatabaseMiddleware.php
+++ src/MessageBus/EshopDatabaseMiddleware.php
@@ -8,6 +8,7 @@ namespace App\MessageBus;
 use App\DBAL\MultiDbConnectionWrapper;
 use App\MessageBus\Stamp\EshopStamp;
 use Doctrine\DBAL\Connection;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Exception\UnrecoverableMessageHandlingException;
 use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
@@ -19,19 +20,24 @@ class EshopDatabaseMiddleware implements MiddlewareInterface
 	 * @param MultiDbConnectionWrapper $connection
 	 */
 	public function __construct(
-		private Connection $connection
+		private Connection      $connection,
+		private ManagerRegistry $managerRegistry,
+		private ?string         $entityManagerName = null
 	) {
 	}
 
 	final public function handle(Envelope $envelope, StackInterface $stack): Envelope
 	{
 		try {
+			$this->connection->connect();
 			/** @var EshopStamp[] $eshopStamps */
 			$eshopStamps = $envelope->all(EshopStamp::class);
 			foreach ($eshopStamps as $eshopStamp) {
 				$this->connection->selectDatabase($eshopStamp->getEshopId());
 			}
 			$envelope = $stack->next()->handle($envelope, $stack);
+			$this->connection->close();
+			$this->managerRegistry->resetManager($this->entityManagerName);
 		} catch (\InvalidArgumentException $e) {
 			throw new UnrecoverableMessageHandlingException($e->getMessage(), 0, $e);
 		}
