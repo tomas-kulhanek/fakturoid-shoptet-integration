56432085b18eaa24c339c598e216c3c0998ad876
Tomáš Kulhánek <jsem@tomaskulhanek.cz>
2021-11-13 16:34:36 +0100

    Oprava aktualizace faktury po zaplaceni proformy

--- src/MessageBus/Handler/DownloadInvoiceMessageHandler.php
+++ src/MessageBus/Handler/DownloadInvoiceMessageHandler.php
@@ -57,7 +57,7 @@ class DownloadInvoiceMessageHandler implements MessageHandlerInterface
 					try {
 						if ($invoice->getProformaInvoice() instanceof ProformaInvoice && $invoice->getProformaInvoice()->getAccountingId() !== null && !$invoice->getProformaInvoice()->isAccountingPaid()) {
 							$this->proformaInvoice->markAsPaid($invoice->getProformaInvoice(), $invoice->getProformaInvoice()->getChangeTime());
-							break;
+							//break;
 						}
 						$this->accountingBusDispatcher->dispatch($invoice);
 					} catch (EmptyLines | FakturoidException) {
