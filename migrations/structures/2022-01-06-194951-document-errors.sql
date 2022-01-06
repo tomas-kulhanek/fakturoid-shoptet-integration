45149ed1822fad9a3890a2f24ba67b2bec2a7bb1
Tomáš Kulhánek <jsem@tomaskulhanek.cz>
2022-01-06 19:50:34 +0100

    migrace pro dokumenty

---
+++ migrations/structures/2022-01-06-194951-document-errors.sql
@@ -0,0 +1,3 @@
+ALTER TABLE sf_credit_note CHANGE accounting_last_errors accounting_last_errors VARCHAR(255) DEFAULT NULL;
+ALTER TABLE sf_proforma_invoice CHANGE accounting_last_errors accounting_last_errors VARCHAR(255) DEFAULT NULL;
+ALTER TABLE sf_invoice CHANGE accounting_last_errors accounting_last_errors VARCHAR(255) DEFAULT NULL;
