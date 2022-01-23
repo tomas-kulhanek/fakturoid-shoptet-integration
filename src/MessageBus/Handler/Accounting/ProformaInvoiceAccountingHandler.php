9520c1d44b331b3e666eb3b9267abef9694c60a9
Tomáš Kulhánek <jsem@tomaskulhanek.cz>
2022-01-23 16:53:26 +0100

    Remove manage clear

--- src/MessageBus/Handler/Accounting/ProformaInvoiceAccountingHandler.php
+++ src/MessageBus/Handler/Accounting/ProformaInvoiceAccountingHandler.php
@@ -23,7 +23,6 @@ class ProformaInvoiceAccountingHandler implements MessageHandlerInterface
 
 	public function __invoke(ProformaInvoice $document): void
 	{
-		$this->entityManager->clear();
 		dump(get_class($document));
 		dump(get_class($this));
 		$project = $this->projectManager->getByEshopId($document->getEshopId());
--- src/MessageBus/Handler/Synchronization/SynchronizeProformaInvoiceHandler.php
+++ src/MessageBus/Handler/Synchronization/SynchronizeProformaInvoiceHandler.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 
 namespace App\MessageBus\Handler\Synchronization;
 
-use App\Database\EntityManager;
 use App\Manager\ProjectManager;
 use App\MessageBus\Message\Synchronization\ProformaInvoiceSynchronizationMessage;
 use App\Synchronization\ProformaInvoiceSynchronization;
@@ -15,14 +14,12 @@ class SynchronizeProformaInvoiceHandler implements MessageHandlerInterface
 {
 	public function __construct(
 		private ProjectManager                 $projectManager,
-		private ProformaInvoiceSynchronization $synchronization,
-		private EntityManager                  $entityManager
+		private ProformaInvoiceSynchronization $synchronization
 	) {
 	}
 
 	public function __invoke(ProformaInvoiceSynchronizationMessage $message): void
 	{
-		$this->entityManager->clear();
 		$project = $this->projectManager->getByEshopId($message->getEshopId());
 		$this->synchronization->synchronize($project, $message->getDateTimeImmutable());
 	}
