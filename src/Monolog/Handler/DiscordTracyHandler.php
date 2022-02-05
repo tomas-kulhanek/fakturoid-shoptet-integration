413a953b0c176541e85d2067a3a435defc3e9cd8
Tomáš Kulhánek <jsem@tomaskulhanek.cz>
2022-02-05 21:25:56 +0100

    Pridani zasilani HTML tracy na discord

---
+++ src/Monolog/Handler/DiscordTracyHandler.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Monolog\Handler;
+
+use DiscordHandler\DiscordHandler;
+use GuzzleHttp\Exception\RequestException;
+use Throwable;
+use Tracy\Debugger;
+use Tracy\Logger;
+
+class DiscordTracyHandler extends DiscordHandler
+{
+	protected function write(array $record): void
+	{
+		try {
+			$logger = new Logger(Debugger::$logDirectory);
+			$exception = $record['context']['exception'] ?? null;
+			if ($exception instanceof Throwable) {
+				parent::write($record);
+				$file = $logger->getExceptionFile($record['context']['exception'], strtolower($record['level_name']));
+				if (!file_exists($file)) {
+					Debugger::getBlueScreen()->renderToFile($exception, $file);
+				}
+
+				$fileName = basename($file);
+				foreach ($this->config->getWebHooks() as $webHook) {
+					$curl = curl_init($webHook);
+					if ($curl === false) {
+						continue;
+					}
+					curl_setopt($curl, CURLOPT_TIMEOUT, 3);
+					curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 3);
+					curl_setopt($curl, CURLOPT_POST, 1);
+					curl_setopt($curl, CURLOPT_HTTPHEADER, [
+						'Content-Type: multipart/form-data'
+					]);
+					curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+					curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+					curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+					curl_setopt($curl, CURLOPT_POSTFIELDS, [
+						'file' => curl_file_create($file, 'text/html', $fileName)
+					]);
+					curl_exec($curl);
+					curl_close($curl);
+				}
+			}
+		} catch (RequestException $e) {
+		}
+	}
+}
