1357d7ed85388ef3e8dafc93a4aee47e4e5ded28
Tomáš Kulhánek <jsem@tomaskulhanek.cz>
2022-03-17 19:04:49 +0100

    Zapracovani security na webhooky

--- src/Modules/Api/Shoptet/ShoptetPresenter.php
+++ src/Modules/Api/Shoptet/ShoptetPresenter.php
@@ -7,6 +7,7 @@ namespace App\Modules\Api\Shoptet;
 
 use App\Database\Entity\Shoptet\Project;
 use App\DTO\Shoptet\Request\Webhook;
+use App\Exception\Logic\NotFoundException;
 use App\Manager\ProjectManager;
 use App\Manager\WebhookManager;
 use App\Mapping\EntityMapping;
@@ -25,7 +26,8 @@ class ShoptetPresenter extends UnsecuredPresenter
 		private WebhookManager              $webhookManager,
 		private EntityMapping               $entityMapping,
 		private InitiatorValidatorInterface $initiatorValidator,
-	) {
+	)
+	{
 		parent::__construct();
 	}
 
@@ -43,7 +45,7 @@ class ShoptetPresenter extends UnsecuredPresenter
 
 	public function actionInstallation(?string $code): void
 	{
-		if ($code === null || $code === '') {
+		if ($code === NULL || $code === '') {
 			$this->error('Forbidden', IResponse::S403_FORBIDDEN);
 		}
 		if (!$this->getRequest()->isMethod('GET')) {
@@ -64,15 +66,19 @@ class ShoptetPresenter extends UnsecuredPresenter
 
 	private function checkSignature(string $webhookBody, Project $project): void
 	{
-		if ($project->getSigningKey() === null || $project->getSigningKey() === '') {
+		if ($project->getSigningKey() === NULL || $project->getSigningKey() === '') {
 			$this->projectManager->renewSigningKey($project);
+		}
+		if ($project->getSigningKey() === NULL || $project->getSigningKey() === '') {
+			Debugger::log(sprintf('Signing key is empty after renewing for project %s', $project->getEshopId()), ILogger::CRITICAL);
+			return;
 		}
 		$calculated = hash_hmac('sha1', $webhookBody, $project->getSigningKey());
 		$expected = $this->getHttpRequest()->getHeader('Shoptet-Webhook-Signature');
 
 		if ($calculated !== $expected) {
 			Debugger::log(sprintf('Signature is not valid for project %s, calculated is %s. Data: %s', $project->getEshopId(), $calculated, $webhookBody), ILogger::CRITICAL);
-			$this->error('Unauthorized', IResponse::S401_UNAUTHORIZED);
+			$this->error('Forbidden', IResponse::S403_FORBIDDEN);
 		}
 	}
 
@@ -83,13 +89,18 @@ class ShoptetPresenter extends UnsecuredPresenter
 		}
 		/** @var Webhook $webhook */
 		$webhook = $this->entityMapping->createEntity($this->getHttpRequest()->getRawBody(), Webhook::class);
-		$project = $this->projectManager->getByEshopId($webhook->eshopId);
-		$this->checkSignature($this->getHttpRequest()->getRawBody(), $project);
-		if ($project->isActive()) {
-			$this->webhookManager->receive($webhook, $project);
-		} else {
-			Debugger::log(sprintf('Received webhook for inactive project %d', $project->getEshopId()), ILogger::WARNING);
+		try {
+			$project = $this->projectManager->getByEshopId($webhook->eshopId);
+		} catch (NotFoundException) {
+			$this->error('Forbidden', IResponse::S403_FORBIDDEN);
 		}
+		$this->checkSignature($this->getHttpRequest()->getRawBody(), $project);
+		if (!$project->isActive()) {
+			Debugger::log(sprintf('Received webhook for inactive project %d', $project->getEshopId()), ILogger::WARNING);
+			$this->error('Forbidden', IResponse::S403_FORBIDDEN);
+		}
+
+		$this->webhookManager->receive($webhook, $project);
 		$this->sendPayload();
 	}
 }
